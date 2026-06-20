5c5896dc9bfc1969bc6886ba07a8cdbc12db2747
Marlo Navarro <marlonavarro12345@gmail.com>
2026-06-20 20:08:56 +0800

    FirstCommit

---
+++ index.php
@@ -0,0 +1,222 @@
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/index.css">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&family=Cause:wght@100..900&family=Cedarville+Cursive&family=Dancing+Script:wght@400..700&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=PT+Sans+Narrow:wght@400;700&family=Playwrite+VN:wght@100..400&family=Saira+Stencil:ital,wght@0,100..900;1,100..900&family=Wix+Madefor+Text:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+   <title>Landing Page</title>
+   </head>
+   <body>
+
+   <header>
+
+      <div class="headerTopPart">
+         <div class="leftSideHeader">
+         <div id="logo">
+            <img src="images/BackToTrack_Logo3.png" alt="">
+         </div>
+         <div id="webTitle">
+            <h1 class="webTitleText">BackToTrack</h1>
+         </div>
+         </div>
+
+         <nav>
+         <button class="active">Home</button>
+         <button>Impact</button>
+         <button>Contact</button>
+         <button>About Us</button>
+
+         <a href="login.php" class="btn">Login</a>
+         <a href="register.php" class="btn">Register</a>
+         </nav>
+      </div>
+
+      <div class="headerBottomPart">
+         <div>
+         <h1>Bridging Skills, Training,<br>and Opportunities.</h1>
+         <p>BackToTrack is a centralized platform <br> that connects out-of-school youth, TESDA,<br>
+         employers, and business for a stronger <br>
+         workforce and a better community.</p>
+
+         <div class="twoStartBtn">
+            <button class="get">
+               Get Started
+            </button>
+            <button class="learn">
+               Learn More
+            </button>
+         </div>
+         </div>
+         <div>
+         <img src="images/big_header_img.png" alt="">
+         </div>
+      </div>
+   </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <div class="midPart">
+      <div class="socialImp">
+         <h1><span class="socialSpan">Social</span> Impact</h1>
+         <p>BackToTrack creates positve change for the Community, Local Business, <br>
+         TESDA, and Baranggay Pogo Grande</p>
+      </div>
+
+      <div class="threeCards">
+
+         <div class="cards">
+         <span class="material-symbols-outlined famGroup">family_group</span>       
+         <h2>Community <br> Development</h2>
+
+   
+         <p>Connects out-of-school youth <br>
+         with training opportunities and <br>
+         employment pathways to reduce <br>
+         unemployment and build a better
+         future</p>
+
+         </div>
+
+         <div class="cards">
+         <span class="material-symbols-outlined integration">integration_instructions</span>
+         <h2>Tesda <br> Integration</h2>
+
+
+         <p>Supports skills training referrals, <br>
+         application tracking, and monitoring <br>
+         of TESDA graduates and programs.</p>
+
+         </div>
+
+         <div class="cards">
+         <span class="material-symbols-outlined groups">groups</span>        
+         <h2>Economic <br> Growth</h2>
+
+
+         <p>Creates opportunities for <br>
+         MSMEs, encourages entrepreneurship, <br>
+         and strengthens the local economy.</p>
+
+         </div>
+      </div>
+      
+      <div class="midPartFooter">
+
+         <div class="dartGoalImg">
+         <img src="images/dart_goal2.png" alt="">
+         </div>
+
+         <div class="ourGoal">
+         <h1>Our Goal</h1>
+         <p>To build a digital ecosystem that bridges training and employment, <br> empowers the youth, and drives socio-economic growth,</p>
+         </div>
+
+         <div class="targetMarketGoal">
+
+         <div class="youth">
+            <span class="material-symbols-outlined person">person</span>
+            <h4>Youth <br> Empowered</h4>
+
+         </div>
+
+         <div class="business">
+            <span class="material-symbols-outlined cases">cases</span>
+            <h4>Businesses <br> Supported</h4>
+
+         </div>
+         
+         <div class="community">
+            <span class="material-symbols-outlined footerGroupsIcon">groups</span>
+            <h4>Stronger <br> Community</h4>
+
+         </div>
+
+         </div>
+
+      </div>
+
+   </div>
+
+
+
+   <footer>
+
+      <div class="footerTopPart">
+
+
+         <div class="footerLogoTitle">
+            <div>
+               <img src="images/BackToTrack_Logo3.png" alt="">
+            </div>
+            <div>
+               <h1>BackToTrack</h1>
+               <p>Let's Build Opportunities.</p>
+            </div>
+
+         </div>
+
+
+         <div class="footerNav">
+            
+            <div class="footerNavLinks">
+               <h1>Quick Links</h1>
+               <a href="">Home</a>
+               <a href="">About</a>
+
+            </div>
+            <div class="footerNavLinks">
+               <h1>Supports</h1>
+               <a href="">Contact</a>
+               <a href="">Socials</a>
+
+            </div>
+            <div class="footerNavLinks">
+               <h1>Partners</h1>
+               <a href="">Tesda</a>
+               <a href="">Pogo Grande</a>
+
+            </div>
+      
+         </div>
+
+         <div class="footerBigLogo">
+            <img src="images/Tesda_Logo4.png" alt="">
+         </div>
+
+
+      </div>
+
+
+
+      <div class="footerBottomPart">
+         <p>© 2026 BackToTrack.</p>
+      </div>
+
+   </footer>
+
+
+
+
+   
+   </body>
+   </html>
---
+++ login.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/login.css">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&family=Cause:wght@100..900&family=Cedarville+Cursive&family=Dancing+Script:wght@400..700&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Lexend:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=PT+Sans+Narrow:wght@400;700&family=Playwrite+VN:wght@100..400&family=Saira+Stencil:ital,wght@0,100..900;1,100..900&family=Wix+Madefor+Text:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+   <title>Login Page</title>
+</head>
+<body>
+      <header>
+
+      <div class="headerTopPart">
+         <div class="leftSideHeader">
+         <div id="logo">
+            <img src="images/BackToTrack_Logo3.png" alt="">
+         </div>
+         <div id="webTitle">
+            <h1 class="webTitleText">BackToTrack</h1>
+         </div>
+         </div>
+
+         <nav>
+         <button>Home</button>
+         <button>Impact</button>
+         <button>Contact</button>
+         <button>About Us</button>
+
+         <a href="login.php" class="active">Login</a>
+         <a href="register.php">Register</a>
+         </nav>
+      </div>
+   </header>
+
+
+
+
+
+
+
+
+
+   <div class="midPart">
+      <div class="loginBox">
+
+         <div class="loginBoxLogoTitle">
+            <div class="loginBoxLogoTitleBackToTrack">
+               <img src="images/BackToTrack_Logo3.png" alt="">
+               <h1 class="webTitleText">BackToTrack</h1>
+            </div>
+            <img src="images/Tesda_Logo4.png" alt="">
+         </div>
+
+
+         <div class="loginText">
+            <h1>Log In</h1>
+            <p>Please fill up the following to log in your account</p>
+         </div>
+
+
+         <div class="inputPart">
+            <p>Email</p>
+            <input type="text" name="email" placeholder="Email">
+         </div>
+
+
+         <div class="inputPart">
+            <p>Password</p>
+            <input type="text" name="password" placeholder="Password">
+         </div>
+
+
+         <div class="keepLogIn">
+            <input type="checkbox" name="keepMeLogin">
+            <p>Keep Me Log In</p>
+         </div>
+
+
+         <div class="signInBtn">
+            <button>Sign In</button>
+         </div>
+
+
+         <div>
+            
+         </div>
+
+      </div>
+   </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <footer>
+
+      <div class="footerTopPart">
+
+
+         <div class="footerLogoTitle">
+            <div>
+               <img src="images/BackToTrack_Logo3.png" alt="">
+            </div>
+            <div>
+               <h1>BackToTrack</h1>
+               <p>Let's Build Opportunities.</p>
+            </div>
+
+         </div>
+
+
+         <div class="footerNav">
+            
+            <div class="footerNavLinks">
+               <h1>Quick Links</h1>
+               <a href="">Home</a>
+               <a href="">About</a>
+
+            </div>
+            <div class="footerNavLinks">
+               <h1>Supports</h1>
+               <a href="">Contact</a>
+               <a href="">Socials</a>
+
+            </div>
+            <div class="footerNavLinks">
+               <h1>Partners</h1>
+               <a href="">Tesda</a>
+               <a href="">Pogo Grande</a>
+
+            </div>
+      
+         </div>
+
+         <div class="footerBigLogo">
+            <img src="images/Tesda_Logo4.png" alt="">
+         </div>
+
+
+      </div>
+
+
+
+      <div class="footerBottomPart">
+         <p>© 2026 BackToTrack.</p>
+      </div>
+
+   </footer>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
